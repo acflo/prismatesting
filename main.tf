e543199ef4042f36f13a87c3ef5140efba3da96a
acflo <98936918+acflo@users.noreply.github.com>
2023-04-02 12:52:51 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["979382823631"] # Bitnami
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-013f88f40e623624b"
+  tags = {
+    Name = "HelloWorld"
+  }
+}

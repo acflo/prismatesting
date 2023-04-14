a1dd36df42bf95e8b6c46b9921b82cd38f594713
acflo <98936918+acflo@users.noreply.github.com>
2023-04-14 14:55:06 +1000

    Update s3main.tf

--- s3main.tf
+++ s3main.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "my-bucket" {
   acl    = "private"
   #checkov:skip=CKV_AWS_18:The bucket does not need logging
   #bridgecrew:skip=999326330578625536_AWS_1679460210128:i want this outside SYDNEY
+  #bridgecrew:skip=BC_AWS_GENERAL_56:I do not need KMS
   tags = {
     Environment = "test"
   }

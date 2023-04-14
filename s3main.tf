049a3195469bcc1135e37821fdd8fbb118de51f4
acflo <98936918+acflo@users.noreply.github.com>
2023-04-14 14:22:05 +1000

    Update s3main.tf

--- s3main.tf
+++ s3main.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "my-bucket" {
   bucket = "my-bucket"
   acl    = "private"
   #checkov:skip=CKV_AWS_18:The bucket does not need logging
+  #bridgecrew:skip=999326330578625536_AWS_1679460210128:i want this outside SYDNEY
   tags = {
     Environment = "test"
   }

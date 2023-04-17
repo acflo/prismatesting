996cfe14d84a7e70f4e79cd98755ede6c9ae5e1c
acflo <98936918+acflo@users.noreply.github.com>
2023-04-17 14:33:14 +1000

    Update s3test.tf

--- s3test.tf
+++ s3test.tf
@@ -4,6 +4,8 @@ provider "aws" {
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "my-bucket"
   acl    = "private"
+  #bridgecrew:skip=999326330578625536_AWS_1680051330670:i want this public
+  #bridgecrew:skip=BC_AWS_GENERAL_56:I do not need KMS
   tags = {
     Environment = "test"
   }

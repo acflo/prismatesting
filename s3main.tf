afb5a99cdb31f1acd493985d68b7151274931f85
acflo <98936918+acflo@users.noreply.github.com>
2023-04-18 14:57:09 +1000

    Update s3main.tf

--- s3main.tf
+++ s3main.tf
@@ -3,6 +3,8 @@ provider "aws" {
 }
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "my-bucket"
+  #bridgecrew:skip=999326330578625536_AWS_1680051330670:I need this public
+  #bridgecrew:skip=BC_AWS_GENERAL_56:I would like to bypass KMS for this test instance
   tags = {
     Environment = "test"
   }

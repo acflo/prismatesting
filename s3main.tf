31c6f02f4f1d1e9add5cd9f9964967183840f956
acflo <98936918+acflo@users.noreply.github.com>
2023-04-19 11:10:31 +1000

    Update s3main.tf

--- s3main.tf
+++ s3main.tf
@@ -1,11 +1,21 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap_southeast-2"
 }
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "my-bucket"
   #bridgecrew:skip=999326330578625536_AWS_1680051330670:I need this public
   #bridgecrew:skip=BC_AWS_GENERAL_56:I would like to bypass KMS for this test instance
   tags = {
-    Environment = "test"
+    tyroTeam = "SECE"
+    tyroDataClassification = "Public"
+    tyroRiskClassification = "High"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "my-bucket" {
+  bucket = aws_s3_bucket.my-bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
   }
 }

355a2f71e051d34b972313c3cd1e17d6eea7aa1f
acflo <98936918+acflo@users.noreply.github.com>
2023-05-29 15:00:54 +1000

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -11,4 +11,12 @@ resource "aws_instance" "example" {
   tags = {
     Name = "aflores-test-machine"
   }
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    sudo systemctl enable amazon-ssm-agent
+    sudo systemctl start amazon-ssm-agent
+  EOF
+  
 }

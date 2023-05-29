5aba5f8fc3e8c4bd912bd60dbcd81e3c00b48581
acflo <98936918+acflo@users.noreply.github.com>
2023-05-29 14:09:05 +1000

    Create ec2instance.tf

---
+++ ec2instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fc20dd1da406780b"  
+  instance_type = "t2.small"               
+  key_name      = "aflorestest"      
+  subnet_id     = "subnet-013f88f40e623624b" 
+  
+  tags = {
+    Name = "aflores-test-machine"
+  }
